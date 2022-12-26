48a48f160db1cdcf5849afcdcdd2e1405032e80d
Sadık Uluçay <sadikulu45@gmail.com>
2022-12-26 19:42:49 +0300

    2022-12-26

--- odev5.js
+++ odev5.js
@@ -23,48 +23,42 @@ const ekle = () => {
     txtName.focus();
     return;
   }
-  // const newLi = document.createElement("tr");
-  newLi = `<tr>
-        <td>${++index}</td>
+  const newLi = document.createElement("tr");
+  newLi.innerHTML = `<td>${++index}</td>
         <td>${txtName.value}</td>
         <td>${txtPoint.value}</td>
         <td>
+        <span class="s1">
           <button id="update">
             <i class="fa-solid fa-pen"></i>
           </button>
           <button id="delete">
             <i class="fa-solid fa-trash-can"></i>
           </button>
-          <button id="tamam" style="display: none;">
+        </span>
+        <span class="s2" style="display: none;">
+          <button id="tamam">
             <i class="fa-solid fa-check"></i>
           </button>
-          <button id="iptal" style="display: none;">
+          <button id="iptal">
             <i class="fa-solid fa-xmark"></i>
           </button>
-        </td>
-        </tr>`;
+        </span>
+        </td>`;
 
-  tblStudentsTbody.innerHTML += newLi;
-  // document.querySelector("#update").style.display = "inline-block";
-  // document.querySelector("#delete").style.display = "inline-block";
-  // document.querySelector("#tamam").style.display = "none";
-  // document.querySelector("#iptal").style.display = "none";
+  tblStudentsTbody.append(newLi);
   toplam += Number(txtPoint.value);
-  /*  let yeni = [];
-  yeni.push(index);
-  let count = yeni[0];
-  console.log(yeni[0]); */
   txtName.value = "";
   txtPoint.value = "";
   txtName.focus();
   sil(toplam, index);
+  iptal(txtName.value, txtPoint.value);
   düzenle(toplam, index);
+  tamam(txtName, txtPoint);
   average(index, toplam);
 };
 btnAdd.addEventListener("click", () => {
   ekle();
-  // document.querySelector("#tamam").style.display = "none";
-  // document.querySelector("#iptal").style.display = "none";
 });
 
 const sil = (toplam, index) => {
@@ -88,30 +82,63 @@ const düzenle = (toplam, index) => {
   document.querySelectorAll("#update").forEach((button) => {
     console.log(button);
     button.onclick = (e) => {
-      /* document.querySelectorAll("#update").forEach((item) => {
-        item.style.display = "none";
-      });
-      document.querySelectorAll("#delete").forEach((item) => {
-        item.style.display = "none";
-      });
-      document.querySelectorAll("#tamam").forEach((item) => {
-        item.style.display = "inline-block";
-      });
-      document.querySelectorAll("#iptal").forEach((item) => {
-        item.style.display = "inline-block";
-      }); */
-      document.getElementById("update").style.display = "none";
-      document.getElementById("delete").style.display = "none";
-      document.getElementById("tamam").style.display = "inline";
-      document.getElementById("iptal").style.display = "inline";
       const trEl = e.target.closest("tr");
-      const name = trEl.querySelector("td:nth-child(2)").innerText;
-      const point = Number(trEl.querySelector("td:nth-child(3)").innerText);
-
+      trEl.querySelector(".s1").style.display = "none";
+      trEl.querySelector(".s2").style.display = "inline-block";
+      const name = trEl.querySelector("td:nth-child(2)");
+      const point = trEl.querySelector("td:nth-child(3)");
+      /* const nameT = trEl.querySelector("td:nth-child(2)");
+      const pointT = trEl.querySelector("td:nth-child(3)"); */
+      const nameEl = trEl.querySelector("td:nth-child(2)").innerText;
+      const pointEl = trEl.querySelector("td:nth-child(3)").innerText;
+      name.innerHTML = `<input type="text" id="name" placeholder=${name.innerText} />`;
+      point.innerHTML = `<input type="number" id="point" placeholder=${point.innerText} />`;
+      const nameT = document.querySelector("#name");
+      const pointT = document.querySelector("#point");
+      iptal(nameEl, pointEl);
+      tamam(nameT, pointT);
       average(index, toplam);
     };
   });
 };
+const iptal = (txtName, txtPoint) => {
+  document.querySelectorAll("#iptal").forEach((button) => {
+    console.log(button);
+    button.onclick = (e) => {
+      const trEl = e.target.closest("tr");
+      trEl.querySelector(".s2").style.display = "none";
+      trEl.querySelector(".s1").style.display = "inline-block";
+      const name = trEl.querySelector("td:nth-child(2)");
+      const point = trEl.querySelector("td:nth-child(3)");
+      name.innerHTML = `<td>${txtName}</td>`;
+      point.innerHTML = `<td>${txtPoint}</td>`;
+      average(index, toplam);
+    };
+  });
+};
+const tamam = (nameT, pointT) => {
+  document.querySelectorAll("#tamam").forEach((button) => {
+    console.log(button);
+    button.onclick = (e) => {
+      const trEl = e.target.closest("tr");
+      trEl.querySelector(".s2").style.display = "none";
+      trEl.querySelector(".s1").style.display = "inline-block";
+      const name = trEl.querySelector("td:nth-child(2)");
+      const point = trEl.querySelector("td:nth-child(3)");
+      name.innerHTML = nameT.innerText;
+      point.innerHTML = pointT.innerText;
+      average(index, toplam);
+    };
+  });
+};
+tamam();
+/* let mode = true;
+document.querySelector("#btn").addEventListener("click", () => {
+  if (mode) {
+  } else {
+  }
+  mode = !mode;
+}); */
 const average = (index, toplam) => {
   ortalama = Math.floor(toplam / index);
   const ort = document.querySelector(".cizgili tfoot td:nth-child(2)");
@@ -120,7 +147,6 @@ const average = (index, toplam) => {
 txtPoint.addEventListener("keyup", (e) => {
   if (e.key == "Enter") {
     ekle();
-    // document.querySelector("#tamam").style.display = "none";
-    // document.querySelector("#iptal").style.display = "none";
+    tamam();
   }
 });
